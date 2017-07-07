6cec489363fa56e7a9ef77453a688a60235e1ade
Alan Hong <xiupan@gmail.com>
2017-07-06 19:35:26 -0500

    Delete Button works! Hooray!

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,18 @@ const Movie = require("../models/Movie")
 
 router.get('/', function(req, res){
   Movie.find()
-  .sort("year")
+  .sort("-year")
   .then(function(movies){
     res.render("index", {
       movies: movies
     })
+  })
+})
+
+router.post('/delete/:id', function(request, response){
+  Movie.deleteOne({"_id": request.params.id})
+  .then(function(deletedMovie){
+    response.redirect('/');
   })
 })
 
